943c3b101b62eb433b758168ff2e783920a37d6c
FELIX-OFC <felixofc3@gmail.com>
2025-10-18 08:05:47 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <title>Página no encontrada - Api Terrarex</title>
-  <link rel="stylesheet" href="/style.css">
-</head>
-<body>
-  <main>
-    <section>
-      <h1>404 - Página no encontrada</h1>
-      <p>Lo sentimos, la página que buscas no existe.</p>
-      <a href="/">Volver al inicio</a>
-    </section>
-  </main>
-</body>
-</html>
+Esta función sin servidor ha fallado.
+
+Su conexión está funcionando correctamente.
+
+Vercel está funcionando correctamente.
+
+500 : ERROR INTERNO DEL SERVIDOR
+Código:FUNCTION_INVOCATION_FAILED
+IDENTIFICACIÓN:iad1::d8564-1760789117201-99928d9852bf
+
+Si es un visitante, comuníquese con el propietario del sitio web o inténtelo nuevamente más tarde.
